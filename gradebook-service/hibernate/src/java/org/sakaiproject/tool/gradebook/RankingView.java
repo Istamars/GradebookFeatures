4bc877b942540fa6517fea0b69b0e109c81b5649
Imar <45618747+Istamars@users.noreply.github.com>
2019-03-10 17:56:47 +0700

    Add files via upload

--- gradebook-service/hibernate/src/java/org/sakaiproject/tool/gradebook/RankingView.java
+++ gradebook-service/hibernate/src/java/org/sakaiproject/tool/gradebook/RankingView.java
@@ -7,4 +7,45 @@ public class RankingView extends GradebookFeedback implements Serializeable{
 	private int 	studentId;
 	private int 	totalPoint;
 	private int 	feedbackId;
+	private int 	uuid;
+
+	public void setNo(int aNo){
+		this.no 		= aNo;
+	}
+
+	public int 	getNo(){
+		return 			no;
+	}
+
+	public void setStudentId(int aStudentId){
+		this.studentId 	= aStudentId;
+	}
+
+	public int 	getStudentId(){
+		return 			studentId;
+	}
+
+	public void setTotalPoint(int aTotalPoint){
+		this.totalPoint = aTotalPoint;
+	}
+
+	public int 	getTotalPoint(){
+		return 			totalPoint;
+	}
+
+	public void setFeedbackId(int aFeedbackId){
+		this.feedbackId = aFeedbackId;
+	}
+
+	public int 	getFeedbackId(){
+		return 			feedbackId;
+	}
+
+	public void setUuid(int aUuid){
+		this.uuid 		= aUuid;
+	}
+
+	public int 	getUuid(){
+		return 			uuid;
+	}
 }
